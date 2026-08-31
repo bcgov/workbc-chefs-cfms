b42d09fcc249101bdf09b5c343717380fb0b4ab6
Branko Bajic <eisoftware@mailfence.com>
2026-08-30 21:48:39 -0700

    remove unused packages for now, try npm install again

--- packages/admin-api/src/routes/permission.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import * as permissionController from "../controllers/permission.controller"
-
-const router = express.Router()
-
-router.get("/", permissionController.getPermission)
-
-export default router
