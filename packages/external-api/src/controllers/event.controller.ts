aa2b09644d028be7a1c9ce5f65cedadda02408e9
Branko Bajic <eisoftware@mailfence.com>
2026-09-25 09:35:43 -0700

    add subscription logic

--- packages/external-api/src/controllers/event.controller.ts
+++ packages/external-api/src/controllers/event.controller.ts
@@ -38,7 +38,7 @@ export const submission = async (req: express.Request, res: express.Response) =>
         }
 
         // Application form events //
-        if (formType === "HaveEmployeeForm" || formType === "NeedEmployeeForm") {
+        if (formType === "PBLMT" || formType === "JCP" || formType === "LMP" || formType === "RI") {
             const application = await applicationService.getApplicationBySubmissionID(req.body.submissionId)
             if (!application) {
                 console.log(
@@ -49,86 +49,6 @@ export const submission = async (req: express.Request, res: express.Response) =>
             if (application?.status === "Draft") {
                 let updateResult
                 if (submissionResponse.submission.draft === false) {
-                    console.log(
-                        `[event.controller] updating submitted application for application id ${application.id} and submission id ${req.body.submissionId}`
-                    )
-                    // Application form has been submitted; determine the catchment & storefront, then update the application in the DB //
-                    let catchment
-                    let storefront
-                    const selected = submission?.data?.otherSelectedCentre ?? submission?.data?.selectedCentre
-                    if (selected && selected.catchment && selected.storefront) {
-                        console.log(
-                            `[event.controller] submission id ${req.body.submissionId} has selected the following catchment & storefront: ${selected.catchment} ${selected.storefront}`
-                        )
-                        catchment = selected.catchment
-                        storefront = selected.storefront
-                    } else {
-                        // Route the catchment & storefront for the submitted application //
-                        // Use the workplace address if provided, otherwise use the business address //
-                        let address
-                        let city
-                        let province
-                        const workplaceContainer = submission?.data?.container
-                        if (
-                            workplaceContainer?.addressAlt &&
-                            workplaceContainer.cityAlt &&
-                            workplaceContainer.provinceAlt
-                        ) {
-                            address = workplaceContainer.addressAlt
-                            city = workplaceContainer.cityAlt
-                            province = workplaceContainer.provinceAlt
-                        } else if (
-                            submission.data.businessAddress &&
-                            submission.data.businessCity &&
-                            submission.data.businessProvince
-                        ) {
-                            address = submission.data.businessAddress
-                            city = submission.data.businessCity
-                            province = submission.data.businessProvince
-                        }
-                        console.log(
-                            `[event.controller] address for submission id ${
-                                req.body.submissionId
-                            } - Address: ${maskAddress(address)}, City: ${city}, Province: ${province}`
-                        )
-                        const { Score, Catchment, Storefront } = await geocoderService.geocodeAddress(
-                            address,
-                            city,
-                            province
-                        )
-                        console.log(
-                            `[event.controller] address validation result for submission id ${req.body.submissionId} - Score: ${Score}, Catchment: ${Catchment}, Storefront: ${Storefront}`
-                        )
-                        if (Score && Catchment && Storefront) {
-                            if (Score >= 80) {
-                                catchment = Catchment
-                                storefront = Storefront
-                            } else {
-                                console.log(
-                                    `[event.controller] insufficient address validation score for application submission id ${req.body.submissionId} - this shouldn't happen!`
-                                )
-                            }
-                        } else {
-                            console.log(
-                                `[event.controller] insufficient results returned from address validation for submission id ${req.body.submissionId} - this shouldn't happen!`
-                            )
-                        }
-                    }
-
-                    if (catchment && storefront) {
-                        const newDataObj = Object.assign(submissionResponse.submission.submission.data, {
-                            catchmentNo: catchment,
-                            storefrontId: storefront,
-                            catchmentNoStoreFront: `${catchment}-${storefront}`,
-                            matchedToCentre: `${catchment}-${storefront}`
-                        })
-                        submissionResponse.submission.submission.data = newDataObj // update the object used for updating the application record
-                    } else {
-                        console.log(
-                            `[event.controller] catchment & storefront calculation failed for submission id ${req.body.submissionId} - this shouldn't happen!`
-                        )
-                    }
-
                     // Update the application //
                     updateResult = await applicationService.updateApplication(
                         application.id,
@@ -136,46 +56,6 @@ export const submission = async (req: express.Request, res: express.Response) =>
                         submissionResponse.submission,
                         false
                     )
-
-                    // If first application submitted, backfill user profile from form data.
-                    const firstApplicationSubmitted = await applicationService.oneApplicationSubmitted(
-                        application.created_by
-                    )
-                    if (firstApplicationSubmitted) {
-                        const user = await userService.getUserByID(application.created_by)
-                        if (!user) {
-                            throw new Error("Internal Server Error")
-                        }
-                        await userService.updateUserFromApplicationForm(
-                            user,
-                            submissionResponse.submission.submission.data
-                        )
-                    }
-
-                    // Update the catchment of the form in CHEFS //
-                    if (catchment) {
-                        await formService.updateSubmissionCatchment(
-                            req.body.submissionId,
-                            submissionResponse.submission,
-                            catchment
-                        )
-                    }
-
-                    // Send notification(s) //
-                    await emailController
-                        .sendEmail(submissionResponse.submission.submission, application.id)
-                        .then(() => {
-                            console.log(
-                                `[event.controller] successfully sent notifications for submission id ${req.body.submissionId}`
-                            )
-                        })
-                        .catch((e) => {
-                            console.log(
-                                `[event.controller] error sending notifications for submission id ${req.body.submissionId} - Error:`,
-                                e
-                            )
-                            return res.status(500).send("Internal Server Error")
-                        })
                 } else if (submissionResponse.submission.draft === true) {
                     console.log(
                         `[event.controller] updating saved application for application id ${application.id} and submission id ${req.body.submissionId}`
@@ -193,7 +73,6 @@ export const submission = async (req: express.Request, res: express.Response) =>
                     )
                     return res.status(200).send()
                 }
-
                 console.log(
                     `[event.controller] unable to update application database entry for application id ${application.id} and submission id ${req.body.submissionId}`
                 )
